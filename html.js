7386cd51d18486bf4a1c572967dcaaf78594f083
HyeonWooGa <yeonwoopark22@gmail.com>
2022-07-30 22:51:34 +0900

    HTML 페이지 1차 완성 (HTML, CSS, JS)

---
+++ html.js
@@ -0,0 +1,44 @@
+const quizFrom = document.getElementById("quiz-form");
+
+const tree = quizFrom.querySelector("#tree");
+const span = quizFrom.querySelector("#span");
+const img = quizFrom.querySelector("#img");
+const a = quizFrom.querySelector("#a");
+const ul = quizFrom.querySelector("#ul");
+const ol = quizFrom.querySelector("#ol");
+const li = quizFrom.querySelector("#li");
+const input = quizFrom.querySelector("#input");
+const textarea = quizFrom.querySelector("#textarea");
+const button = quizFrom.querySelector("#button");
+const semantic = quizFrom.querySelector("#semantic");
+const search = quizFrom.querySelector("#search");
+const footer = quizFrom.querySelector("#footer");
+const header = quizFrom.querySelector("#header");
+const nav = quizFrom.querySelector("#nav");
+const main = quizFrom.querySelector("#main");
+const id = quizFrom.querySelector("#id");
+const classs = quizFrom.querySelector("#classs");
+
+function handleSubmit(event) {
+  event.preventDefault();
+  if (tree.value !== "트리") tree.value = "";
+  if (span.value !== "<span>") span.value = "";
+  if (img.value !== "<img>") img.value = "";
+  if (a.value !== "<a>") a.value = "";
+  if (ul.value !== "<ul>") ul.value = "";
+  if (ol.value !== "<ol>") ol.value = "";
+  if (li.value !== "<li>") li.value = "";
+  if (input.value !== "<input>") input.value = "";
+  if (textarea.value !== "<textarea>") textarea.value = "";
+  if (button.value !== "<button>") button.value = "";
+  if (semantic.value !== "시맨틱") semantic.value = "";
+  if (search.value !== "검색") search.value = "";
+  if (footer.value !== "<footer>") footer.value = "";
+  if (header.value !== "<header>") header.value = "";
+  if (nav.value !== "<nav>") nav.value = "";
+  if (main.value !== "<main>") main.value = "";
+  if (id.value !== "id") id.value = "";
+  if (classs.value !== "class") classs.value = "";
+}
+
+quizFrom.addEventListener("submit", handleSubmit);
