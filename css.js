73c823c08b62bcb0d35f551522c1d945301323c5
heoputer <hjw1753@gmail.com>
2022-08-01 21:26:17 +0900

    정답 오답 효과 적용

--- css.js
+++ css.js
@@ -4,15 +4,46 @@ const flex = quizFrom.querySelector("#flex");
 const flexDirection = quizFrom.querySelector("#flex-direction");
 const justifyContent = quizFrom.querySelector("#justify-content");
 const alignItem = quizFrom.querySelector("#align-item");
-
 const button = document.querySelector("button");
+
+const inputData = [flex, flexDirection, justifyContent, alignItem, button]
+const answer = ["flex", "flex-direction", "justify=content", "align-item", "button"]
+
+
+// 정답 채점 함수 만들기.
+// 아래 함수에서 this를 어떻게 사용하면 해결이 될 듯 한데.. 고민중입니다 ㅠ
+
+// function grading(input, answer) {
+//   if (input.value === answer) flex.classList.add('success');
+//   else {
+//     flex.classList.add('Fail');
+//     flex.value = "오답입니다.";
+//   }
+// }
 
 function handleSubmit(event) {
   event.preventDefault();
-  if (flex.value !== "flex") flex.value = "";
-  if (flexDirection.value !== "flex-direction") flexDirection.value = "";
-  if (justifyContent.value !== "justify-content") justifyContent.value = "";
-  if (alignItem.value !== "align-item") alignItem.value = "";
+
+  if (flex.value !== "flex") {
+    flex.classList.add('Fail');
+    flex.value = "오답입니다.";
+  } else flex.classList.add('success');
+
+  if (flexDirection.value !== "flex-direction") {
+    flexDirection.classList.add('Fail');
+    flexDirection.value = "오답입니다.";
+  } else flexDirection.classList.add('success');
+
+  if (justifyContent.value !== "justify-content") {
+    justifyContent.classList.add('Fail');
+    justifyContent.value = "오답입니다.";
+  } else justifyContent.classList.add('success');
+
+  if (alignItem.value !== "align-item") {
+    alignItem.classList.add('Fail');
+    alignItem.value = "오답입니다.";
+  } else alignItem.classList.add('success');
+
   button.style.backgroundColor = "#E2DCC8";
   button.style.color = "#5a8f7b";
   setTimeout(() => {
