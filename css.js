2f370375ed182a6d8b808a252c79a4580fad2a8f
heoputer <hjw1753@gmail.com>
2022-08-02 20:20:23 +0900

    빈칸 효과 적용 완료

--- css.js
+++ css.js
@@ -6,49 +6,47 @@ const justifyContent = quizFrom.querySelector("#justify-content");
 const alignItem = quizFrom.querySelector("#align-item");
 const button = document.querySelector("button");
 
-// const inputData = [flex, flexDirection, justifyContent, alignItem, button]
-// const answer = ["flex", "flex-direction", "justify=content", "align-item", "button"]
-
-
-// 정답 채점 함수 만들기.
-// 아래 함수에서 this를 어떻게 사용하면 해결이 될 듯 한데.. 고민중입니다 ㅠ
-
-// function grading(input, answer) {
-//   if (input.value === answer) input.classList.add('success');
-//   else {
-//     input.classList.add('Fail');
-//     input.value = "오답입니다.";
-//   }
-// }
+const inputData = [flex, flexDirection, justifyContent, alignItem, button]
+const answer = ["flex", "flex-direction", "justify=content", "align-item", "button"]
 
 function handleSubmit(event) {
   event.preventDefault();
 
-  // grading(inputData, answer)
   if (flex.value === "flex") flex.classList.add('success');
     else if (flex.value === "") {
       flex.classList.add('blank');
       flex.value= "빈칸입니다.";
-    }
-    else {
+    } else {
       flex.classList.add('Fail');
       flex.value = "오답입니다.";
     }
 
-  if (flexDirection.value !== "flex-direction") {
+  if (flexDirection.value === "flex-direction")  flexDirection.classList.add('success');
+    else if (flexDirection.value ==="") {
+      flexDirection.classList.add('blank');
+      flexDirection.value= "빈칸입니다.";
+    } else {
     flexDirection.classList.add('Fail');
     flexDirection.value = "오답입니다.";
-  } else flexDirection.classList.add('success');
+    }
 
-  if (justifyContent.value !== "justify-content") {
+  if (justifyContent.value === "justify-content") justifyContent.classList.add('success');
+    else if(justifyContent.value ==="") {
+      justifyContent.classList.add('blank');
+      justifyContent.value= "빈칸입니다.";
+  } else {
     justifyContent.classList.add('Fail');
     justifyContent.value = "오답입니다.";
-  } else justifyContent.classList.add('success');
+  }
 
-  if (alignItem.value !== "align-item") {
+  if (alignItem.value === "align-item") alignItem.classList.add('success');
+    else if (alignItem.value === "") {
+      alignItem.classList.add('blank');
+      alignItem.value="빈칸입니다.";
+    } else {
     alignItem.classList.add('Fail');
     alignItem.value = "오답입니다.";
-  } else alignItem.classList.add('success');
+    }
 
   button.style.backgroundColor = "#E2DCC8";
   button.style.color = "#5a8f7b";
