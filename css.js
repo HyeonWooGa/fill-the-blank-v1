b6107dfb8431dff5c4eac2010e3b438aa8394ca7
heoputer <hjw1753@gmail.com>
2022-08-02 23:25:34 +0900

    인풋 벨류 초기화 구현

--- css.js
+++ css.js
@@ -12,15 +12,27 @@ const answer = ["flex", "flex-direction", "justify-content", "align-item", "butt
 function grading(input, answer) {
   for(let i = 0; i < input.length; i++) {
     if(input[i].value === answer[i]) {
-      inputData[i].classList.add("success")
+      inputData[i].classList.remove("blank");
+      inputData[i].classList.remove("Fail");
+      inputData[i].classList.add("success");
     } else if (input[i].value === "") {
+      inputData[i].classList.remove("success")
+      inputData[i].classList.remove("Fail")
       inputData[i].classList.add("blank")
       inputData[i].value = "빈칸입니다.";
     } else {
+      inputData[i].classList.remove("success");
+      inputData[i].classList.remove("blank");
       inputData[i].classList.add("Fail");
       inputData[i].value = "오답입니다.";
     }
   }
+}
+function inputWipe(event) {
+  event.target.value = "";
+  event.target.classList.remove("success")
+  event.target.classList.remove("blank")
+  event.target.classList.remove("Fail")
 }
 
 function handleSubmit(event) {
@@ -36,4 +48,9 @@ function handleSubmit(event) {
   }, 10);
 }
 
+flex.addEventListener("click", inputWipe);
+flexDirection.addEventListener("click", inputWipe);
+justifyContent.addEventListener("click", inputWipe);
+alignItem.addEventListener("click", inputWipe);
+button.addEventListener("click", inputWipe);
 quizFrom.addEventListener("submit", handleSubmit);
